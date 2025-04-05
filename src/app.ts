5f22c2c41deef5039d631ef45a90dc2a9f4d6f74
nakanoyuki <yukich41@gmail.com>
2025-04-05 23:12:46 +0900

    入力項目を取得して内側からアクセス

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ class ProjectInput {
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
 
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
+
   // テンプレートや表示する要素取得
   constructor() {
     this.templeteElement = document.getElementById(
@@ -13,10 +17,32 @@ class ProjectInput {
     // 画面の表示で使うimportされたNode
     const importNode = document.importNode(this.templeteElement.content, true);
     this.element = importNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    //入力項目を取得して内側からアクセス
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.mandayInputElement = this.element.querySelector(
+      "#manday"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
   private attach() {
+    //同じコンテキストでthisを参照している　同じクラスのオブジェクト
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
